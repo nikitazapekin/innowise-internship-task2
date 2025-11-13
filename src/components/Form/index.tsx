ac87d9be746eb8df845107e8857231f3c82e4edb
Global Name <zapekinnikitamain@gmail.com>
2025-11-13 23:18:09 +0300

    feat(husku): Update husky

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,25 +1,20 @@
 import { Container } from "@mui/material";
-import theme from "@styles/theme";
-import { useTheme } from "@mui/material/styles";
- 
+
 const Form = () => {
-  const theme = useTheme();
   return (
     <>
       <Container
         component="main"
-    
         sx={{
           minHeight: "100vh",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          maxWidth: "100%", 
-          width: "100%", 
-        
+          maxWidth: "100%",
+          width: "100%",
         }}
       >
-        efwfewewf
+        efwfewewfd
       </Container>
     </>
   );
